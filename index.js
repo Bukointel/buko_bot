e101933cd3ed1f86340a6b5cd7c4d196256b038f
ivandalechenko <ivandalechenko@gmail.com>
2025-01-19 12:48:24 +0200

    added .envexample

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
+require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = 'meow';
+const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Hello!');
-});
-
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    if (!msg.text.startsWith('/')) {
-        bot.sendMessage(chatId, `Ypu say: "${msg.text}"`);
-    }
 });
 
 console.log('Bot started!');
